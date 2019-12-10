6e5594bddb2aeb258f53ce20646c5f4d47ade287
chris <christopher.fajardo73@gmail.com>
2019-12-10 01:53:58 -0500

    small

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ const style = {
   top: 0,
   left: 0,
   zIndex: -1,
-  opacity: 0.8
+  opacity: 0.87,
+  backgroundColor: "black"
 };
 class App extends React.Component {
   render() {
@@ -23,7 +24,6 @@ class App extends React.Component {
     };
     return (
       <>
-        {" "}
         <Navbar></Navbar>
         <div style={style}>
           <Cover
@@ -36,20 +36,28 @@ class App extends React.Component {
             }}
           />
         </div>
-        <div className="h-100">
-          <div className="container text-center w-100">
-            <div className="row">
-              <div className="col-md-12 m-auto text-white text-center">
-                <h1 className="display-4"> DEli</h1>
-              </div>
-            </div>{" "}
-            <svg className="arrows">
-              <path className="a1" d="M0 0 L30 32 L60 0"></path>
-              <path className="a2" d="M0 20 L30 52 L60 20"></path>
-              <path className="a3" d="M0 40 L30 72 L60 40"></path>
-            </svg>
+        <div className="h-100 container text-center w-100 d-flex flex-column justify-content-center align-items-center">
+          <div className="row">
+            <div className="col-md-12 m-auto text-white text-center">
+              <h1 className="display-2 text-uppercase font-weight-bold">
+                Nonis<br></br> Coffee Shop
+              </h1>{" "}
+              <button className="btn btn-lg btn-outline-light p-2 m-5 font-weight-bold">
+                ORDER ONLINE
+              </button>
+              <h3 className="text-uppercase ">
+                5921 Riverdale Ave<br></br> Bronx, NY 10471
+              </h3>
+              <h3 className="text-uppercase my-3 ">(718) 601-0132</h3>
+            </div>
           </div>
+          <svg className="arrows">
+            <path className="a1" d="M0 0 L30 32 L60 0"></path>
+            <path className="a2" d="M0 20 L30 52 L60 20"></path>
+            <path className="a3" d="M0 40 L30 72 L60 40"></path>
+          </svg>
         </div>
+
         <div className="container ">
           <div className="row">
             <div className="col-md-12 m-auto text-black text-center">
---
+++ src/components/About.js
@@ -0,0 +1,3 @@
+// Sun 8:00 AM - 8:00 PM
+// Mon-Thu 9:00 AM - 8:00 PM
+// Fri-Sat 8:00 AM - 8:00 P
